5439f087a8d74ed6547322257b39c7c5b4a8177c
rianto21 <orytomikli21@gmail.com>
2026-05-30 16:04:39 +0700

    tts test

--- app/api/tts/route.ts
+++ app/api/tts/route.ts
@@ -1,17 +1,25 @@
 import { GoogleGenAI } from "@google/genai";
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function POST(req: Request) {
   try {
+    const { text } = await req.json();
+
     const ai = new GoogleGenAI({
       apiKey: process.env.GEMINI_API_KEY!,
     });
 
-    const models = await ai.models.list();
+    const response = await ai.models.generateContent({
+      model: "gemini-3.1-flash-tts-preview",
+      contents: text,
+      config: {
+        responseModalities: ["TEXT"],
+      },
+    });
 
     return NextResponse.json({
       success: true,
-      models,
+      result: response.text,
     });
   } catch (error: any) {
     return NextResponse.json({
